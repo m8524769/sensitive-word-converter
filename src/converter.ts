97d2b4256b259675b37e8736ef58f2f7065fb34f
m8524769 <m8524769@163.com>
2019-03-26 23:32:34 +0800

    Add validate() & Simplify the codes
    - validate(source: string): Object
    - return {
    -   pass: boolean,
    -   sensitiveWords: Set
    - }

--- src/converter.ts
+++ src/converter.ts
@@ -4,15 +4,14 @@ import * as readline from 'readline';
 export class Converter {
 
   private sensitiveWordMap: Map<string, any> = new Map();
-  private filePath: string = 'sensitiveWords.txt';
-  private lineReader = readline.createInterface({
-    input: fs.createReadStream(
-      this.filePath, { encoding: 'UTF-8' }
-    )
-  });
 
-  constructor() {
-    this.lineReader.on('line', line => {
+  constructor(filePath: string = 'sensitiveWords.txt') {
+    const lineReader = readline.createInterface({
+      input: fs.createReadStream(
+        filePath, { encoding: 'UTF-8' }
+      )
+    });
+    lineReader.on('line', line => {
       if (line) {
         let subMap = this.sensitiveWordMap;
         for (let c of line) {
@@ -28,33 +27,59 @@ export class Converter {
     })
   }
 
-  convert(source: string, substitute: string = '*'): string {
-    // console.log(this.sensitiveWordMap);
-    let target: string = source;
+  validate(source: string): Object {
+    let pass: boolean = true;
+    let sensitiveWords: Set<string> = new Set();
     for (let i = 0; i < source.length; i++) {
-      let subMap = this.sensitiveWordMap;
-      if (!subMap.has(source[i])) {
-        continue;
-      } else {
-        // console.log(source[i])
-        subMap = subMap.get(source[i]);
-        if (subMap.get('isEnd') === true) {  // Single character
-          console.log('found ' + source[i]);
-          console.log('replaceTo ' + substitute);
-          target = target.replace(source[i], substitute);
-          console.log(target);
+      if (this.sensitiveWordMap.has(source[i])) {
+        let subMap = this.sensitiveWordMap.get(source[i]);
+        if (subMap.get('isEnd')) {  // Single character
+          sensitiveWords.add(source[i]);
+          pass = false;
         }
         for (let j = i+1; j < source.length; j++) {
           if (subMap.has(source[j])) {
             subMap = subMap.get(source[j]);
-            if (subMap.get('isEnd') === true) {
-              console.log('found ' + source.substring(i, j+1));
-              console.log('replaceTo ' + substitute.repeat(j-i+1));
+            if (subMap.get('isEnd')) {
+              sensitiveWords.add(source.substring(i, j + 1));
+              pass = false;
+              break;
+            }
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    return {
+      "pass": pass,
+      "sensitiveWords": sensitiveWords
+    };
+  }
+
+  convert(source: string, substitute: string = '*'): string {
+    // console.log(this.sensitiveWordMap);
+    let target: string = source;
+    for (let i = 0; i < source.length; i++) {
+      if (source[i] != substitute && this.sensitiveWordMap.has(source[i])) {
+        let subMap = this.sensitiveWordMap.get(source[i]);
+        if (subMap.get('isEnd')) {  // Single character
+          console.log('Found ' + source[i]);
+          console.log('ReplaceTo ' + substitute);
+          target = target.replace(source[i], substitute);
+          console.log('Result ' + target);
+        }
+        for (let j = i+1; j < source.length; j++) {
+          if (subMap.has(source[j])) {
+            subMap = subMap.get(source[j]);
+            if (subMap.get('isEnd')) {
+              console.log('Found ' + source.substring(i, j + 1));
+              console.log('ReplaceTo ' + substitute.repeat(j - i + 1));
               target = target.replace(
                 source.substring(i, j + 1),
                 substitute.repeat(j - i + 1)
               );
-              console.log(target);
+              console.log('Result ' + target);
               break;
             }
           } else {
--- tests/converter.test.ts
+++ tests/converter.test.ts
@@ -2,20 +2,37 @@ import { Converter } from '../src';
 import { expect } from 'chai';
 
 describe("Converter test", () => {
-  const converter = new Converter();
-  it("Test1 success", () => {
+
+  const converter = new Converter('sensitiveWords.txt');
+
+  it("Test 0", () => {
+    const source = 'hhhhhhhychhh';
+    const target = 'hhhhhh***hhh';
+    expect(converter.convert(source)).to.equal(target);
+  });
+  it("Test 1", () => {
     const source = 'hello worlds';
     const target = '**llo *****s';
     expect(converter.convert(source)).to.equal(target);
   });
-  it("Test2 success", () => {
+  it("Test 2", () => {
     const source = '哈哈哈哈啪！';
     const target = '喵喵喵喵啪！';
     expect(converter.convert(source, '喵')).to.equal(target);
   });
-  it("Test3 success", () => {
+  it("Test 3", () => {
     const source = '你好骚啊！';
     const target = '****！';
     expect(converter.convert(source)).to.equal(target);
   });
+  it("Validate Testing", () => {
+    const source = 'hello worlds';
+    const target = '**llo *****s';
+    const returnValue = converter.validate(source);
+    console.log(returnValue);
+    expect(returnValue).to.include({
+      pass: false,
+    });
+  });
+
 });
