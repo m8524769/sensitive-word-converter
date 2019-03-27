bb8c3ff27bb478a6162e3cd0e19e09304cc27b15
m8524769 <m8524769@163.com>
2019-03-27 19:48:56 +0800

    Add comments for each method in index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,25 @@
 export declare class Converter {
 
+  /**
+   *
+   * @param filePath A text file containing sensitive words. (one word for each line)
+   */
   constructor(filePath: string)
 
-  convert(source: string, substitute: string): string
-  convert(source: string): string
+  /**
+   * Returns a string whose sensitive words were replaced by substitutes.
+   * @param source The string you want to convert.
+   * @param substitute The character used to replace sensitive word, Default: '*'.
+   */
+  convert(source: string, substitute?: string): string
+
+  /**
+   * Return an Object {
+   *   pass: boolean,
+   *   sensitiveWords: Set {}
+   * }
+   * @param source A string to check for sensitive words inside.
+   */
   validate(source: string): Object
 
 }
