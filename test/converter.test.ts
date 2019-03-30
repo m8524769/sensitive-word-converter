b1dae083fad5a3401077ccedc1fe261e0a46741b
m8524769 <m8524769@163.com>
2019-03-30 23:30:47 +0800

    Style: modify something in test file

--- test/converter.test.ts
+++ test/converter.test.ts
@@ -3,35 +3,38 @@ import { expect } from 'chai';
 
 describe("Converter test", () => {
 
-  const converter = new Converter('sensitiveWords.txt');
+  // Only support local file test
+  const converter = new Converter('./sensitiveWords.txt');
 
   it("Test 0", () => {
     const source = 'hhhhhhhychhh';
     const target = 'hhhhhh***hhh';
     expect(converter.convert(source)).to.equal(target);
   });
+
   it("Test 1", () => {
     const source = 'hello worlds';
     const target = '**llo *****s';
     expect(converter.convert(source)).to.equal(target);
   });
+
   it("Test 2", () => {
     const source = '哈哈哈哈啪！';
     const target = '喵喵喵喵啪！';
     expect(converter.convert(source, '喵')).to.equal(target);
   });
+
   it("Test 3", () => {
     const source = '你好骚啊！';
     const target = '****！';
     expect(converter.convert(source)).to.equal(target);
   });
-  it("Validate Testing", () => {
+
+  it("Validation Test", () => {
     const source = 'hello worlds';
-    const target = '**llo *****s';
     const returnValue = converter.validate(source);
-    console.log(returnValue);
     expect(returnValue).to.include({
-      pass: false,
+      pass: false
     });
   });
 
