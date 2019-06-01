46b80d764ede6884d6f2e78b10e7418eb61d39aa
yk <m8524769@163.com>
2019-06-01 16:35:17 +0800

    Update unit test

--- test/converter.test.ts
+++ test/converter.test.ts
@@ -1,22 +1,21 @@
 import { Converter } from '../src';
 import { expect } from 'chai';
 
-describe('Converter', () => {
+interface TestSuite {
+  name: string;
+  testCases: TestCase[];
+}
 
-  let cvt: Converter;
+interface TestCase {
+  name: string;
+  args: string[];
+  expect: string;
+}
 
-  before(async () => {
-    await new Converter(
-      './sensitiveWords.txt',
-      'https://raw.githubusercontent.com/fwwdn/sensitive-stop-words/master/%E5%B9%BF%E5%91%8A.txt'
-    ).isReady.then(converter => {
-      cvt = converter;
-    });
-  });
-
-  describe('#convert()', () => {
-
-    let tests = [
+const TEST_SUITES: TestSuite[] = [
+  {
+    name: 'Test Suite #1',
+    testCases: [
       {
         name: 'he, world',
         args: ['hello world'],
@@ -33,22 +32,42 @@ describe('Converter', () => {
         expect: '****！'
       },
     ]
+  },
+  {
+    name: 'Test Suite #2',
+    testCases: [
+      {
+        name: 'hyc -> _',
+        args: ['cccchycccc', '_'],
+        expect: 'cccc___ccc'
+      },
+    ]
+  },
+];
 
-    tests.forEach(test => {
-      it(test.name, () => {
-        expect(cvt.convert.apply(cvt, test.args))
-          .to.equal(test.expect);
-      })
-    })
+describe('Converter', () => {
 
+  let cvt: Converter;
+
+  before(async () => {
+    await new Converter(
+      './sensitiveWords.txt',
+      'https://raw.githubusercontent.com/fwwdn/sensitive-stop-words/master/%E5%B9%BF%E5%91%8A.txt'
+    ).isReady.then(converter => {
+      cvt = converter;
+    });
   });
 
-  describe('#validate()', () => {
-    it('hello world', () => {
-      expect(cvt.validate('hello world'))
-        .to.include({
-          pass: false
+  describe('#convert()', () => {
+    TEST_SUITES.forEach(testSuite => {
+      describe(testSuite.name, () => {
+        testSuite.testCases.forEach(testCase => {
+          it(testCase.name, () => {
+            expect(cvt.convert.apply(cvt, testCase.args))
+              .to.equal(testCase.expect);
+          });
         });
+      });
     });
   });
 
